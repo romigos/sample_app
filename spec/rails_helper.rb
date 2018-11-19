50c59693da24c3a06d8dcdcf1f81610aead66cb9
remka <bolist@mail.ru>
2018-11-19 22:59:12 +0200

    make new controller user

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,24 +22,36 @@ require 'rspec/rails'
 #
 # Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
+<% if RSpec::Rails::FeatureCheck.can_maintain_test_schema? -%>
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
-  puts e.to_s.strip
-  exit 1
+puts e.to_s.strip
+exit 1
+   end
+<% elsif RSpec::Rails::FeatureCheck.can_check_pending_migrations? -%>
+# Checks for pending migrations before tests are run.
+# If you are not using ActiveRecord, you can remove these lines.
+begin
+  ActiveRecord::Migration.check_pending!
+rescue ActiveRecord::PendingMigrationError => e
+puts e.to_s.strip
+exit 1
 end
+<% end -%>
 RSpec.configure do |config|
-  config.include Capybara::DSL, :type => :feature
-   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+<% if RSpec::Rails::FeatureCheck.has_active_record? -%>
+# Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
+# If you're not using ActiveRecord, or you'd prefer not to run each of your
+# examples within a transaction, remove the following line or assign false
+# instead of true.
+config.use_transactional_fixtures = true
 
+<% end -%>
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
@@ -59,5 +71,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
 end
