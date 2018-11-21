0ca254e7738b87ec0bd92a1afe054d8a58d97e76
remka <bolist@mail.ru>
2018-11-21 23:25:56 +0200

    make model user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+
+  it { should be_valid }
+
+  describe "when name is not present" do
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
 end
